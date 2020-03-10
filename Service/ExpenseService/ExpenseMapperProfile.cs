cb770abb5113238bdbf211be0b0d377aa0cb85f5
Michael Zhang <mzhang@gflenv.com>
2020-03-10 16:03:54 -0400

    expenseAPI

---
+++ Service/ExpenseService/ExpenseMapperProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Entity;
+using ExpenseSystem.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseSystem.Service.ExpenseService
+{
+    public class ExpenseMapperProfile : Profile
+    {
+        public ExpenseMapperProfile()
+        {
+            CreateMap<Expense, ExpenseDTO>()
+                .ForMember(d => d.ExpenseType, o => o.Ignore());
+
+            CreateMap<ExpenseDTO, Expense>()
+                .ForMember(d => d.ExpenseType, o => o.Ignore());
+        }
+    }
+}
